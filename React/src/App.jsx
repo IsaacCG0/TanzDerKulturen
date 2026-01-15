5b2e9231884f497f331808b1c16e66dda03eb313
IsaacCG0 <isaaccercos2006@gmail.com>
2026-01-15 11:54:16 +0100

    Tailwind-test

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -7,12 +7,45 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="flex justify-center items-center h-screen bg-slate-900">
-      <h1 className="text-3xl font-bold text-white underline">
-        ¡Hola Mundo desde React!
-      </h1>
+    <div className='flex flex-nowrap h-screen w-full'>
+
+    <div className="flex flex-col justify-center h-screen bg-slate-900 w-1/4">
+      
+
+      <div className="w-full">
+        <h1 className="text-3xl font-bold text-white underline p-4">
+          ¡Hola Mundo desde React!
+        </h1>
+      </div>
+
+      <div className="w-full">
+        <p className='text-white p-4'>
+          Estoy probando tailwind
+        </p>
+      </div>
+
+    </div>
+
+    <div className="flex flex-col justify-center h-screen bg-amber-50 w-3/4">
+      
+
+      <div className="w-full">
+        <h1 className="text-3xl font-bold underline p-4">
+          ¡Hola Mundo desde React!
+        </h1>
+      </div>
+
+      <div className="w-full">
+        <p className=' p-4'>
+          Estoy probando tailwind
+        </p>
+      </div>
+
+    </div>
+
     </div>
   )
 }
+
 
 export default App
